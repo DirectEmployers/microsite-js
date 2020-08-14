8131d3a2a7cf396857e60f094f0fbae264e33a63
Angeline Tran <angeline.e.tran@gmail.com>
2020-08-14 12:23:11 -0400

    fix screen reader issues for autocomplete

--- src/components/Form/AppAutocompleteInput.vue
+++ src/components/Form/AppAutocompleteInput.vue
@@ -1,65 +1,72 @@
 <template>
-<div class="form__autocomplete">
-    <label
-        v-if="label"
-        class="form__label"
-        :id="`form__label-${id}`"
-        :for="`form__autocomplete-${id}`">
-            {{ label }}
-    </label>
-    <input
-        :id="`form__autocomplete-${id}`"
-        ref="input"
-        v-bind="$attrs"
-        class="form__input"
-        type="text"
-        :value="value"
-        @input="changeValue($event.target.value)"
-        @blur="blur"
-        @keydown.enter.prevent="keyEnter"
-        @keydown.tab="keyEnter"
-        @keydown.up="keyUp"
-        @keydown.down="keyDown"
-        aria-autocomplete="list"
-        aria-haspopup="listbox"
+    <div 
+        class="form__autocomplete" 
         role="combobox"
-        aria-controls="form__autocomplete-results"
-        :aria-labelledby="`form__label-${id}`"
+        aria-haspopup="listbox"
+        :aria-owns="`form__autocomplete-items-${id}`"
         :aria-expanded="isExpanded"
-        :aria-activedescendant="activeDesendent"
     >
-    <div
-        v-if="loading"
-        class="form__autocomplete--loading spinner spinner--gray"
-    ></div>
-
-    <div
-        v-show="results.length"
-        class="form__autocomplete-results"
-    >
-        <ul
-            class="form__autocomplete-items"
-            id="`form__autocomplete-items-${id}`"
-            role="listbox"
+        <label
+            v-if="label"
+            class="form__label"
+            :id="`form__label-${id}`"
+            :for="`form__autocomplete-${id}`"
         >
-            <template v-for="(result, index) in results">
-                <slot name="result" :result="result">
-                    <li
-                        class="form__autocomplete-item"
-                        :ref="`option-${index}`"
-                        :key="index"
-                        :id="`form__autocomplete--${id}-${index}`"
-                        :class="{'form__autocomplete-item--active': index === selectedIndex}"
-                        @mouseover="selectedIndex = index"
-                        @click="setValue(result)"
-                    >
-                        {{ result[display] }}
-                    </li>
-                </slot>
-            </template>
-        </ul>
+            {{ label }}
+        </label>
+        <input
+            :id="`form__autocomplete-${id}`"
+            ref="input"
+            v-bind="$attrs"
+            class="form__input"
+            type="text"
+            :value="value"
+            @input="changeValue($event.target.value)"
+            @blur="blur"
+            @keydown.enter.prevent="keyEnter"
+            @keydown.tab="keyEnter"
+            @keydown.up="keyUp"
+            @keydown.down="keyDown"
+            aria-autocomplete="list"
+            aria-haspopup="listbox"
+            aria-controls="form__autocomplete-results"
+            :aria-labelledby="`form__label-${id}`"
+            :aria-activedescendant="activeDesendent"
+        />
+        <div
+            v-if="loading"
+            class="form__autocomplete--loading spinner spinner--gray"
+        ></div>
+
+        <div v-show="results.length" class="form__autocomplete-results">
+            <ul
+                class="form__autocomplete-items"
+                :id="`form__autocomplete-items-${id}`"
+                role="listbox"
+            >
+                <template v-for="(result, index) in results">
+                    <slot name="result" :result="result">
+                        <li
+                            class="form__autocomplete-item"
+                            :ref="`option-${index}`"
+                            :key="index"
+                            :id="`form__autocomplete--${id}-${index}`"
+                            :class="{
+                                'form__autocomplete-item--active':
+                                    index === selectedIndex,
+                            }"
+                            @mouseover="selectedIndex = index"
+                            @click="setValue(result)"
+                            role="option" 
+                            :aria-selected="activeDesendent"
+                        >
+                            {{ result[display] }}
+                        </li>
+                    </slot>
+                </template>
+            </ul>
+        </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -75,10 +82,9 @@ export default {
                 return `${this._uid}`
             },
         },
-        siteConfig:{
+        siteConfig: {
             type: Object,
-            required: true
-
+            required: true,
         },
         value: String,
         label: String,
@@ -86,8 +92,8 @@ export default {
         display: {
             type: String,
             required: false,
-            default: "display"
-        }
+            default: "display",
+        },
     },
     data() {
         return {
@@ -95,11 +101,11 @@ export default {
             results: [],
             result: null,
             loading: false,
-            error: null
+            error: null,
         }
     },
     methods: {
-        doSearch: debounce( async function (value) {
+        doSearch: debounce(async function(value) {
             this.loading = true
             if (value.length < 2) {
                 this.loading = false
@@ -114,22 +120,22 @@ export default {
                 this.loading = false
             }
         }, 200),
-        changeValue (value) {
+        changeValue(value) {
             this.$emit("input", value)
             this.selectedIndex = -1
             this.doSearch(value)
         },
-        blur (event) {
+        blur(event) {
             this.$emit("input", event.target.value)
-            setTimeout( () => this.results = [], 200)
+            setTimeout(() => (this.results = []), 200)
         },
-        setValue (result) {
+        setValue(result) {
             this.$emit("input", result[this.display])
             this.result = result
             this.$emit("setResult", result)
         },
-        keyEnter () {
-            if(this.selectedIndex !== -1) {
+        keyEnter() {
+            if (this.selectedIndex !== -1) {
                 this.setValue(this.results[this.selectedIndex])
                 this.results = []
                 this.selectedIndex = -1
@@ -139,7 +145,7 @@ export default {
                 this.$emit("setResult", null)
             }
         },
-        keyUp () {
+        keyUp() {
             if (this.selectedIndex > -1) {
                 this.selectedIndex--
             }
@@ -148,7 +154,7 @@ export default {
             }
             this.scroll()
         },
-        keyDown () {
+        keyDown() {
             if (this.selectedIndex <= this.results.length) {
                 this.selectedIndex++
             }
@@ -160,17 +166,23 @@ export default {
         scroll() {
             const option = this.$refs[`option-${this.selectedIndex}`]
             if (option) {
-                option[0].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+                option[0].scrollIntoView({
+                    behavior: "smooth",
+                    block: "nearest",
+                    inline: "start",
+                })
             }
-        }
+        },
     },
     computed: {
-        isExpanded () {
+        isExpanded() {
             return this.results.length ? "true" : "false"
         },
-        activeDesendent () {
-            return this.selectedIndex > -1 ? `form__autocomplete--${this.id}-${this.selectedIndex}` : ""
-        }
-    }
+        activeDesendent() {
+            return this.selectedIndex > -1
+                ? `form__autocomplete--${this.id}-${this.selectedIndex}`
+                : ""
+        },
+    },
 }
 </script>
