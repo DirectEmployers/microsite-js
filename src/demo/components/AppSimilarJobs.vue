dfd35342d60e7f07a2a97e8f1cf3556eddf10d34
Sergio Compean <scompean24@gmail.com>
2020-08-24 14:57:28 -0400

    update similar jobs demo component

--- src/demo/components/AppSimilarJobs.vue
+++ src/demo/components/AppSimilarJobs.vue
@@ -1,53 +1,58 @@
 <template>
-    <AppSearchProvider
-        ref="provider"
-        :search-on-load="false"
-        :site-config="$siteConfig"
-        v-slot="{
-            jobs,
-            meta,
-        }"
-        class="similar-jobs container mx-auto"
-    >
-        <div class="mx-4" v-if="meta.hasJobs">
-            <h3 class="similar-jobs__title">Other Jobs You Might Like</h3>
-            <div class="similar-jobs__grid">
-                <g-link
-                    class="similar-jobs__grid-item"
-                    v-for="( similarJob , index) in jobs"
-                    :key="index"
-                     v-if="similarJob.getGuid() != job.getGuid()"
-                    :to="similarJob.getDetailUrl()"
-                >
-                     <h4 class="similar-jobs__grid-item-title">
-                        {{ similarJob.getTitle() }}
-                    </h4>
-                    <p class="similar-jobs__grid-item-location">
-                        {{ job.getLocation() }}
-                    </p>
-                </g-link>
-            </div>
+    <div class="mx-4" v-if="hasSimilarJobs">
+        <h3 class="similar-jobs__title">Other Jobs You Might Like</h3>
+        <div class="similar-jobs__grid">
+            <g-link
+                v-for="(similarJob, index) in similarJobs"
+                :key="index"
+                :to="similarJob.getDetailUrl()"
+                class="similar-jobs__grid-item"
+            >
+                <h4 class="similar-jobs__grid-item-title">
+                    {{ similarJob.getTitle() }}
+                </h4>
+                <p class="similar-jobs__grid-item-location">
+                    {{ job.getLocation() }}
+                </p>
+            </g-link>
         </div>
-    </AppSearchProvider>
+    </div>
 </template>
 
 <script>
-import AppSearchProvider from "~/components/Search/AppSearchProvider"
+import { SearchService } from "microsite-js/src/services/api/search"
 
 export default {
-    props: ["job"],
-    components: {
-        AppSearchProvider,
+    data() {
+        return {
+            similarJobs: [],
+        }
     },
+    props: ["job"],
     mounted() {
         this.getJobs()
     },
+    computed: {
+        hasSimilarJobs() {
+            return this.similarJobs.length > 0
+        },
+    },
     methods: {
         async getJobs() {
-            await this.$refs["provider"].search({
-                num_items: 10,
-                q: this.job.getTitle(),
-                location: this.job.getLocation(),
+            const response = await SearchService.get(
+                {
+                    num_items: 10,
+                    q: this.job.getTitle(),
+                    location: this.job.getLocation(),
+                },
+                this.$siteConfig
+            )
+
+            const { jobs } = response.data
+            this.similarJobs = jobs
+
+            this.similarJobs.filter(job => {
+                return job.getGuid() !== this.job.getGuid()
             })
         },
     },
