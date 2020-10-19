e4072682ae86235ef5e62c21c4ed0ad2a70fc07d
Sergio Compean <scompean24@gmail.com>
2020-10-19 11:16:31 -0400

    build files

--- dist/microsite-js.common.js
+++ dist/microsite-js.common.js
@@ -1039,14 +1039,14 @@ module.exports = ''.repeat || function repeat(count) {
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7ee05290-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Search/AppSearchFilter.vue?vue&type=template&id=62e4d94c&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7ee05290-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Search/AppSearchFilter.vue?vue&type=template&id=0dd856d2&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return (_vm.displayedOptions.length && _vm.isVisible)?_c('AppAccordion',{key:(_vm.keyName + "-accordion"),staticClass:"search-filter",attrs:{"open":_vm.isActive,"name":(_vm.keyName + "-accordion")},scopedSlots:_vm._u([{key:"header",fn:function(ref){
 var isOpen = ref.isOpen;
 return [_vm._t("display",[(_vm.display)?_c('h3',{staticClass:"search-filter-display",class:{'search-filter-display--active': _vm.isActive}},[_vm._v(" Filter By "+_vm._s(_vm.display)+" ")]):_vm._e()],{"isOpen":isOpen})]}}],null,true)},[_c('ul',{staticClass:"search-filter-options"},_vm._l((_vm.displayedOptions),function(option,index){return _c('li',{key:index,staticClass:"search-filter-options-item"},[_c('g-link',{attrs:{"to":option.href}},[_vm._v(" "+_vm._s(option.display)+" "),(option.value)?_c('span',[_vm._v("("+_vm._s(option.value)+")")]):_vm._e()])],1)}),0),(_vm.shouldShowLess || _vm.hasMoreItems)?_c('section',{staticClass:"search-filter-limiter"},[(_vm.hasMoreItems)?_c('button',{staticClass:"search-filter-limiter-more",attrs:{"aria-label":"Show more filters","rel":"nofollow"},on:{"click":function($event){return _vm.showMore()}}},[_vm._v(" More ")]):_vm._e(),(_vm.shouldShowLess)?_c('button',{staticClass:"search-filter-limiter-less",attrs:{"aria-label":"Show less filters","rel":"nofollow"},on:{"click":function($event){return _vm.showLess()}}},[_vm._v(" Less ")]):_vm._e()]):_vm._e()]):_vm._e()}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/Search/AppSearchFilter.vue?vue&type=template&id=62e4d94c&
+// CONCATENATED MODULE: ./src/components/Search/AppSearchFilter.vue?vue&type=template&id=0dd856d2&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.for-each.js
 var es_array_for_each = __webpack_require__("4160");
@@ -1219,6 +1219,7 @@ var lodash = __webpack_require__("2ef0");
 //
 //
 //
+//
 
 
 
@@ -1279,9 +1280,8 @@ var lodash = __webpack_require__("2ef0");
       return this.visible !== false;
     },
     shouldShowLess: function shouldShowLess() {
-      var numberOfItemsToAdd = 10;
       var totalItemsShown = this.displayedOptions.length;
-      var hasItemsAdded = totalItemsShown > numberOfItemsToAdd;
+      var hasItemsAdded = totalItemsShown > this.limit;
 
       if (hasItemsAdded) {
         return true;
@@ -1303,7 +1303,7 @@ var lodash = __webpack_require__("2ef0");
   methods: {
     buildFilterHref: function buildFilterHref(option) {
       var params = this.cleanedInput;
-      params['page'] = 1;
+      params["page"] = 1;
       params[this.name] = option.submitValue;
       return "/jobs?" + Object(helpers["e" /* toQueryString */])(params);
     },
@@ -1316,7 +1316,7 @@ var lodash = __webpack_require__("2ef0");
       var display = option.display;
       var submitValue = option.display;
 
-      if (Object.prototype.hasOwnProperty.call(option, 'submitValue')) {
+      if (Object.prototype.hasOwnProperty.call(option, "submitValue")) {
         submitValue = option.submitValue;
       }
 
@@ -1352,14 +1352,20 @@ var lodash = __webpack_require__("2ef0");
       return filtered;
     },
     showMore: function showMore() {
-      var numberOfItemsToAdd = 10;
+      var numberOfItemsToAdd = this.limit;
       var currentTotalShown = this.displayedOptions.length;
       this.displayedOptions = this.filteredOptions.slice(0, currentTotalShown + numberOfItemsToAdd);
     },
     showLess: function showLess() {
-      var numberOfItemsToAdd = 10;
+      var limit = this.limit;
       var currentTotalShown = this.displayedOptions.length;
-      this.displayedOptions = this.filteredOptions.slice(0, currentTotalShown - numberOfItemsToAdd);
+      var less = currentTotalShown - limit;
+
+      if (less < limit) {
+        less = currentTotalShown - less;
+      }
+
+      this.displayedOptions = this.filteredOptions.slice(0, less);
     }
   }
 });
