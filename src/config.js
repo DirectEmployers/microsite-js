1a8ab4a96ee174b740aaf0769f7c5b6ecfa1d3cf
Sergio Compean <scompean24@gmail.com>
2020-12-04 10:02:09 -0500

    config update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config =  {
     // buids: [50611, 50613],
     buids:[19424],
-    source: "solr",
+    source: "google_talent",
     project_id: process.env.GRIDSOME_GOOGLE_TALENT_PROJECT_ID,
     tenant_uuid: process.env.GRIDSOME_GOOGLE_TALENT_TENANT,
     company_uuids: [process.env.GRIDSOME_GOOGLE_TALENT_COMPANY],
