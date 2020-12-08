e461857706a6e1c15febc654b07e2c209ff7d8d1
Nathaniel Lyttle <nate998877@gmail.com>
2020-12-08 16:07:44 -0500

    finished boilterplate

---
+++ src/components/AppButtonPagination.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav>
+        <button
+            type="button"
+            @click
+        >
+            More
+        </button>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        currentPage: {
+            required: false,
+            type: Number,
+            default: 1,
+        },
+        totalPages: {
+            required: true,
+            type: Number,
+        }
+    },
+    data() {
+        return {
+            current : this.currentPage
+        }
+    },
+    computed: {
+        nextPage() {
+            const next = this.current + 1
+
+            return next <= this.totalPages ? next : false
+        },
+        jobList() {
+            return this.jobs
+        }
+    }
+}
+</script>
