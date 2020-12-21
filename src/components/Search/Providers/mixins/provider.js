7d547e328153024eddab0562bec6aee5d993d5c0
Nathaniel Lyttle <nate998877@gmail.com>
2020-12-21 15:22:46 -0500

    fixed bug where jobs didn't load when sort option changed

--- src/components/Search/Providers/mixins/provider.js
+++ src/components/Search/Providers/mixins/provider.js
@@ -1,9 +1,9 @@
 import {blank, displayLocationFromSlug} from "../../../../services/helpers"
 import {omitBy, clone, startCase} from "lodash"
-import {searchService, SOLR, GOOGLE_TALENT} from '../../../../services/search'
+import {searchService, SOLR, GOOGLE_TALENT} from "../../../../services/search"
 
-export default  {
-    props:{
+export default {
+    props: {
         siteConfig: {
             required: true,
             type: Object,
@@ -11,7 +11,7 @@ export default  {
         isLoadMore: {
             required: false,
             type: Boolean,
-            default: false
+            default: false,
         },
         tag: {
             required: false,
@@ -26,13 +26,15 @@ export default  {
         defaultInput: {
             required: false,
             type: Object,
-            default: () => { return {} },
+            default: () => {
+                return {}
+            },
         },
     },
     data() {
         return {
             meta: {
-                source: this.siteConfig.source
+                source: this.siteConfig.source,
             },
             status: {
                 loading: false,
@@ -51,7 +53,7 @@ export default  {
             input: this.getInputDefaults(),
         }
     },
-    computed:{
+    computed: {
         service() {
             return searchService
         },
@@ -70,21 +72,23 @@ export default  {
         },
         sort() {
             let sort = {
+                by: null,
                 options: [],
-                sortField: ()=>{},
-                by: null
+                sortField: () => {},
             }
 
             let sortMeta = clone(this.meta.sort || {})
             if (blank(sortMeta)) {
                 return sort
             }
-            sort.sortField = (field) => {
+
+            sort.by = startCase(sortMeta.active)
+            sort.options = sortMeta.options.map(o => startCase(o))
+            sort.sortField = field => {
                 this.input.sort = field.toLowerCase()
                 this.newSearch()
             }
-            sort.by = blank(sortMeta) ? "" : startCase(sortMeta.active)
-            sort.options = sortMeta.options.map(o => startCase(o))
+
             return sort
         },
     },
@@ -99,95 +103,110 @@ export default  {
             let isLessThanMaxPage = this.input.page < this.tmpData.page
             let isNextPage = this.tmpData.lastPage < this.input.page
 
-            if(isLessThanMaxPage && isNextPage){
-                this.displayedJobs = this.displayedJobs.concat(this.tmpData.nextPage.splice(0, this.num_items))
+            let prefetched = this.tmpData.prefetched
+            if (isLessThanMaxPage && isNextPage) {
+                //loading previously loaded page, 
+                this.displayedJobs = this.displayedJobs.concat(
+                    prefetched.splice(0, this.num_items)
+                )
                 this.pagination.page = this.input.page
-            } else if(isLessThanMaxPage && this.isNotNewQuery()) {
-                let offset = this.num_items
-                offset = this.displayedJobs.length - offset
+            } else if (isLessThanMaxPage && !this.isNewQuery()) {
+                //go back button used, handles updating pages & removing jobs
                 this.pagination.page = this.input.page
                 this.tmpData.lastPage = this.input.page
+
+                let offset = this.displayedJobs.length - this.num_items
                 let removedJobs = this.displayedJobs.splice(offset)
-                this.tmpData.nextPage = removedJobs.concat(this.tmpData.nextPage)
+                prefetched = removedJobs.concat(prefetched)
             } else {
-                if(!this.isNotNewQuery()){
-                    this.tmpData.nextPage = []
+                if (this.isNewQuery()) {
+                    prefetched = []
                     this.displayedJobs = []
                     this.isFirstLoad = true
                 }
                 this.updateTmpData()
-                this.search().then( () => {
-                    if(this.isLoadingMore){
+                this.search().then(() => {
+                    if (this.isLoadingMore) {
                         this.isLoadingMore = false
                     }
                 })
             }
         },
     },
-    created(){
+    created() {
         this.input = this.mergeWithDefaultInput({
             ...this.$route.query,
-            ...this.$route.params
+            ...this.$route.params,
         })
 
         this.updateTmpData()
 
-        if(!blank(this.$route.params.location)){
-            this.input.location =  displayLocationFromSlug(this.input.location)
+        if (!blank(this.$route.params.location)) {
+            this.input.location = displayLocationFromSlug(this.input.location)
         }
 
-        if(this.searchOnLoad) {
+        if (this.searchOnLoad) {
             this.search()
         }
     },
-    methods:{
-        queryChanged(){},
-        applyFilters(){
+    methods: {
+        queryChanged() {},
+        applyFilters() {
             return []
         },
 
-        isNotNewQuery(){
-            let allowed = ["location", "travelDuration", "roadTraffic", "title", "shift"]
-            for(let key in this.input){
-                if(allowed.includes(key)){
-                    if(!(this.tmpData.query[key] === this.input[key])){
-                        return false
+        isNewQuery() {
+            let allowed = [
+                "location",
+                "travelDuration",
+                "roadTraffic",
+                "title",
+                "shift",
+                "sort"
+            ]
+            for (let key in this.input) {
+                if (allowed.includes(key)) {
+                    if (!(this.tmpData.query[key] === this.input[key])) {
+                        return true
                     }
                 }
             }
-            return true
+            return false
         },
-        updateTmpData(){
+        updateTmpData() {
             this.tmpData.query = {
                 location: this.input.location,
                 travelDuration: this.input.travelDuration,
                 roadTraffic: this.input.roadTraffic,
                 title: this.input.title,
                 shift: this.input.shift,
+                sort: this.input.sort
             }
         },
         loadMore(page) {
-            if(page){
+            if (page) {
                 this.isLoadingMore = true
                 this.input.page = page
                 this.pushPayload()
             } else {
-                this.displayedJobs = this.displayedJobs.concat(this.tmpData.nextPage)
-                this.tmpData.nextPage = []
+                this.displayedJobs = this.displayedJobs.concat(
+                    this.tmpData.prefetched
+                )
+                this.tmpData.prefetched = []
             }
         },
         mergeWithDefaultInput(from = {}) {
             return {
                 ...this.getInputDefaults(),
-                ...from
+                ...from,
             }
         },
-        removeFilter(params){
+        removeFilter(params) {
             const defaultInput = this.getInputDefaults()
             if (params == "*") {
                 return this.pushPayload({})
             }
-            if(!Array.isArray(params)){
+            if (!Array.isArray(params)) {
                 params = [params]
             }
 
@@ -195,19 +214,22 @@ export default  {
                 this.input[key] = defaultInput[key] || ""
             })
 
-            if(params.includes('location')){
-                this.input['coords'] = defaultInput['coords']
-                this.input['r'] = defaultInput['r']
+            if (params.includes("location")) {
+                this.input["coords"] = defaultInput["coords"]
+                this.input["r"] = defaultInput["r"]
             }
             this.newSearch()
         },
-        getAppliedFilters(){
+        getAppliedFilters() {
             let filters = []
             let added = []
             const input = this.input
             this.configFilters.forEach(filter => {
                 let params = [filter.name]
-                if (!blank(input[filter.name]) && !added.includes(filter.name)) {
+                if (
+                    !blank(input[filter.name]) &&
+                    !added.includes(filter.name)
+                ) {
                     filters.push({
                         display: input[filter.name],
                         parameter: params,
@@ -218,72 +240,88 @@ export default  {
             return filters.concat(this.applyFilters())
         },
         search() {
-            if(!this.isLoadingMore){
+            if (!this.isLoadingMore) {
                 this.status.loading = true
             }
-            if(this.input.offset){
+            if (this.input.offset) {
                 delete this.input.offset
             }
 
             //load pages n - n-5 on first load.
-            if(this.isFirstLoad && this.isLoadMore){
+            if (this.isFirstLoad && this.isLoadMore) {
                 this.tmpData.page = parseInt(this.input.page)
 
                 this.tmpData.num_items = this.num_items
-                this.input.num_items = this.tmpData.num_items * this.input.page + this.tmpData.num_items
-                if(this.input.num_items > 90){
+                this.input.num_items =
+                    this.tmpData.num_items * this.input.page +
+                    this.tmpData.num_items
+                if (this.input.num_items > 90) {
                     this.input.offset = this.input.num_items - 90
                     this.input.num_items = 90
                 }
                 this.input.page = 1
-            } else if(this.isLoadMore) {
+            } else if (this.isLoadMore) {
                 this.tmpData.lastPage = this.$route.query.page
-                this.displayedJobs = this.displayedJobs.concat(this.tmpData.nextPage)
+                this.displayedJobs = this.displayedJobs.concat(
+                    this.tmpData.prefetched
+                )
             }
 
-            return this.service(this.input, this.siteConfig).then(resp=>{
-                const data = resp.data || {}
-                this.featuredJobs = data.featured_jobs || []
-                this.pagination = data.pagination || {}
-                this.filters = data.filters || {}
-                this.jobs = data.jobs || []
-                this.meta = data.meta || {'source': SOLR} //prevents sites from erroring when unable to connect to api
-                this.appliedFilters = this.getAppliedFilters()
-            }).catch( err => {
-                this.status.error = err
-            }).finally(() =>{
-                this.status.loading = false
-                if(!this.isFirstLoad && this.isLoadMore){
-                    this.tmpData.page = this.input.page > this.tmpData.page ? this.input.page : this.tmpData.page
-                    this.tmpData.nextPage = this.jobs
-                }
-
-                if(this.isFirstLoad){
-                    this.isFirstLoad = false
-
-                    if(this.isLoadMore){
-                        this.input.num_items = this.tmpData.num_items
-                        if(this.jobs.length >= 30){
-                            this.tmpData.nextPage = this.jobs.splice(this.jobs.length - this.num_items)
-                        } else if(this.jobs.length < 30){
-                            this.tmpData.nextPage = this.jobs.splice(15)
-                        }
-                        this.displayedJobs = this.jobs
-                        this.pagination.page = this.tmpData.page
-                        this.input.page = this.tmpData.page
-                        //this line keep the load more button from disappearing when first load is large
-                        //eg page loaded is 8, but pagination returns 3 total due to large num_items
-                        this.pagination.total_pages = Math.ceil(parseInt(this.pagination.total) / this.num_items)
+            return this.service(this.input, this.siteConfig)
+                .then(resp => {
+                    const data = resp.data || {}
+                    this.featuredJobs = data.featured_jobs || []
+                    this.pagination = data.pagination || {}
+                    this.filters = data.filters || {}
+                    this.jobs = data.jobs || []
+                    this.meta = data.meta || {source: SOLR} //prevents sites from erroring when unable to connect to api
+                    this.appliedFilters = this.getAppliedFilters()
+                })
+                .catch(err => {
+                    this.status.error = err
+                })
+                .finally(() => {
+                    this.status.loading = false
+                    if (!this.isFirstLoad && this.isLoadMore) {
+                        this.tmpData.page =
+                            this.input.page > this.tmpData.page
+                                ? this.input.page
+                                : this.tmpData.page
+                        this.tmpData.prefetched = this.jobs
                     }
-                }
-            })
+
+                    if (this.isFirstLoad) {
+                        this.isFirstLoad = false
+
+                        if (this.isLoadMore) {
+                            this.input.num_items = this.tmpData.num_items
+                            if (this.jobs.length >= 30) {
+                                this.tmpData.prefetched = this.jobs.splice(
+                                    this.jobs.length - this.num_items
+                                )
+                            } else if (this.jobs.length < 30) {
+                                this.tmpData.prefetched = this.jobs.splice(15)
+                            }
+                            this.displayedJobs = this.jobs
+                            this.pagination.page = this.tmpData.page
+                            this.input.page = this.tmpData.page
+                            //this line keep the load more button from disappearing when first load is large
+                            //eg page loaded is 8, but pagination returns 3 total due to large num_items
+                            this.pagination.total_pages = Math.ceil(
+                                parseInt(this.pagination.total) / this.num_items
+                            )
+                        }
+                    }
+                })
         },
         getFilterOptions(filter) {
             let key = blank(filter.key) ? filter.name : filter.key
-            return (blank(this.filters[key]) || !Array.isArray(this.filters[key])) ? [] : this.filters[key]
+            return blank(this.filters[key]) || !Array.isArray(this.filters[key])
+                ? []
+                : this.filters[key]
         },
 
-        setFilter(key, value){
+        setFilter(key, value) {
             this.input[key] = value
             this.newSearch()
         },
@@ -301,7 +339,7 @@ export default  {
                     name: "jobs",
                     path: "/jobs",
                     query: payload,
-                    params: {savePos: true}
+                    params: {savePos: true},
                 })
                 .catch(err => {})
         },
@@ -309,8 +347,8 @@ export default  {
             return this.filterEmpty(this.input)
         },
         filterEmpty(data, callback = null) {
-            if(callback === null){
-                callback =  () => false
+            if (callback === null) {
+                callback = () => false
             }
             return omitBy(data, (v, k) => {
                 return blank(v) || callback(k, v)
@@ -328,14 +366,12 @@ export default  {
                     sort: "relevance",
                 },
                 ...this.inputDefaults(),
-                ...defaultInput
+                ...defaultInput,
             }
         },
         newSearch() {
             this.input.page = 1
             this.pushPayload()
         },
-
-    }
+    },
 }
-
