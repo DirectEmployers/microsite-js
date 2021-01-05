9803b059dcd60fae53ca5a5286d6918b57d571aa
Sergio Compean <scompean24@gmail.com>
2021-01-05 11:29:44 -0500

    remove comment

--- src/services/search.js
+++ src/services/search.js
@@ -10,7 +10,6 @@ let API_URL = "https://qc-search-api.jobsyn.org/api/v1/"
 if (process.env.GRIDSOME_USE_MINIKUBE === "true") {
     API_URL = "http://minikube:35000/api/v1"
 } else if (!isDevelopment()) {
-    //update whenever we have a prod version.
     API_URL = "https://prod-search-api.jobsyn.org/api/v1/"
 }
 
