dc44ba6f301aee561959bc3a53610cc79e792693
nate <nate998877@gmail.com>
2021-02-05 10:23:14 -0500

    removed file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config =  {
     buids: [19424],
-    source: "google_talent",
+    source: "solr",
     s3Folder: "sanfordhealth-jobs",
     project_id: process.env.GRIDSOME_GOOGLE_TALENT_PROJECT_ID,
     tenant_uuid: process.env.GRIDSOME_GOOGLE_TALENT_TENANT,
