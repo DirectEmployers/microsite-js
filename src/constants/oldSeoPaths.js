d7dd387bbc914a3a19f50894b6c727db4591b462
nate <nate998877@gmail.com>
2021-03-29 15:10:03 -0400

    unborked the bork

--- src/constants/oldSeoPaths.js
+++ src/constants/oldSeoPaths.js
@@ -1,17 +1,23 @@
-const oldSeoPaths = {
-    TITLE: ["/:title/jobs-in"],
-    LOCATION: [
+const oldSeoPaths = [
+    [
         "/:country/jobs",
         "/:state/:country/jobs",
         "/:city/:state/:country/jobs"
     ],
-    MOC: ["/:mocTitle/:moc/:mocBranch/vet-jobs"],
-    FILTERS: [
+    [
+        "/:title/jobs-in"
+    ],
+    [
+        "/:mocTitle/:moc/:mocBranch/vet-jobs"
+    ],
+    [
         "/:filter0/new-jobs",
         "/:filter0/:filter1/new-jobs",
         "/:filter0/:filter1/:filter2/new-jobs"
     ],
-    COMPANY: ["/:company/careers"]
-}
+    [
+        "/:company/careers"
+    ]
+]
 
 module.exports = oldSeoPaths
